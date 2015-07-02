eb7fa53da656801b9fa040db8dcf5b44622f9cae
Elena Ezhova <eezhova@mirantis.com>
2015-07-02 15:52:36 +0300

    Increase test coverage of systemd
    Added UT to cover the following functions:
      * _abstractify
      * notify
      * notify_once
      * onready
    
    Removed usage of mox in test_systemd.
    
    Change-Id: I58fd367250b18c3045ff7f2242f3084ee84bb948

--- oslo_service/tests/test_systemd.py
+++ oslo_service/tests/test_systemd.py
@@ -15,43 +15,64 @@
 import os
 import socket
 
+import mock
 from oslotest import base as test_base
-from oslotest import moxstubout
 
 from oslo_service import systemd
 
 
 class SystemdTestCase(test_base.BaseTestCase):
     """Test case for Systemd service readiness."""
-    def setUp(self):
-        super(SystemdTestCase, self).setUp()
-        self.mox = self.useFixture(moxstubout.MoxStubout()).mox
-        self.stubs = self.useFixture(moxstubout.MoxStubout()).stubs
 
-    def test_sd_notify(self):
+    def test__abstractify(self):
+        sock_name = '@fake_socket'
+        res = systemd._abstractify(sock_name)
+        self.assertEqual('\0{0}'.format(sock_name[1:]), res)
+
+    @mock.patch.object(os, 'getenv', return_value='@fake_socket')
+    def _test__sd_notify(self, getenv_mock, unset_env=False):
         self.ready = False
+        self.closed = False
 
-        def mock_getenv(key):
-            return '@fake_socket'
+        class FakeSocket(object):
+            def __init__(self, family, type):
+                pass
 
-        def mock_socket(cls, *args):
+            def connect(fs, socket):
+                pass
 
-            class FakeSocket(object):
-                def connect(fs, socket):
-                    pass
+            def close(fs):
+                self.closed = True
 
-                def close(fs):
-                    pass
+            def sendall(fs, data):
+                if data == 'READY=1':
+                    self.ready = True
 
-                def sendall(fs, data):
-                    if (data == 'READY=1'):
-                        self.ready = True
+        with mock.patch.object(socket, 'socket', new=FakeSocket):
+            if unset_env:
+                systemd.notify_once()
+            else:
+                systemd.notify()
 
-            return FakeSocket()
+            self.assertTrue(self.ready)
+            self.assertTrue(self.closed)
 
-        self.stubs.Set(os, 'getenv', mock_getenv)
-        self.stubs.Set(socket, 'socket', mock_socket)
+    def test_notify(self):
+        self._test__sd_notify()
 
-        systemd.notify()
+    def test_notify_once(self):
+        os.environ['NOTIFY_SOCKET'] = '@fake_socket'
+        self._test__sd_notify(unset_env=True)
+        self.assertRaises(KeyError, os.environ.__getitem__, 'NOTIFY_SOCKET')
 
-        self.assertEqual(self.ready, True)
+    @mock.patch("socket.socket")
+    def test_onready(self, sock_mock):
+        recv_results = ['READY=1', '', socket.timeout]
+        expected_results = [0, 1, 2]
+        for recv, expected in zip(recv_results, expected_results):
+            if recv == socket.timeout:
+                sock_mock.return_value.recv.side_effect = recv
+            else:
+                sock_mock.return_value.recv.return_value = recv
+            actual = systemd.onready('@fake_socket', 1)
+            self.assertEqual(expected, actual)
