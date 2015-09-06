56b3ff0d388d590a4bc28b1560991a0ee6b3844a
apporc <appleorchard2000@gmail.com>
2015-09-06 12:20:52 +0800

    ThreadGroup's stop didn't recognise the current thread correctly
    Because x is a custom Thread instance, and threading.current_thread()
    returns a GreenThread instance, they will never be a same instance.
    
    This patch add one more property method for Thread class to identify itself.
    
    Change-Id: I173a0a40b417d613cd270d42e40295075cd5b71f
    Signed-off-by: apporc <appleorchard2000@gmail.com>
    (cherry picked from commit 9e81bbae059b5432a47d8bc7a2059c5132e2a11a)

--- oslo_service/tests/test_threadgroup.py
+++ oslo_service/tests/test_threadgroup.py
@@ -19,6 +19,8 @@ Unit Tests for thread groups
 
 import time
 
+from eventlet import event
+
 from oslotest import base as test_base
 
 from oslo_service import threadgroup
@@ -46,6 +48,25 @@ class ThreadGroupTestCase(test_base.BaseTestCase):
         self.assertTrue(timer._running)
         self.assertEqual(('arg',), timer.args)
         self.assertEqual({'kwarg': 'kwarg'}, timer.kw)
+
+    def test_stop_current_thread(self):
+
+        stop_event = event.Event()
+        quit_event = event.Event()
+
+        def stop_self(*args, **kwargs):
+            if args[0] == 1:
+                time.sleep(1)
+                self.tg.stop()
+                stop_event.send('stop_event')
+            quit_event.wait()
+
+        for i in range(0, 4):
+            self.tg.add_thread(stop_self, i, kwargs='kwargs')
+
+        stop_event.wait()
+        self.assertEqual(1, len(self.tg.threads))
+        quit_event.send('quit_event')
 
     def test_stop_immediately(self):
 
