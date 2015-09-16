cb7ea40e31d559d2a5c910d06d008e50b8af82ca
lin-hua-cheng <os.lcheng@gmail.com>
2015-09-15 17:19:29 -0700

    Move backoff looping call from IPA to oslo.service
    Change-Id: Icf61e73a2b28c800b1725c8154aa3044395616e5
    Depends-On: I15ae3c99267b2dd9dc9ceccd427f6c0aef6ae8da

--- oslo_service/tests/test_loopingcall.py
+++ oslo_service/tests/test_loopingcall.py
@@ -235,6 +235,86 @@ class DynamicLoopingCallTestCase(test_base.BaseTestCase):
         sleep_mock.assert_has_calls([mock.call(3), mock.call(1)])
 
 
+class TestBackOffLoopingCall(test_base.BaseTestCase):
+    @mock.patch('random.SystemRandom.gauss')
+    @mock.patch('eventlet.greenthread.sleep')
+    def test_exponential_backoff(self, sleep_mock, random_mock):
+        def false():
+            return False
+
+        random_mock.return_value = .8
+
+        self.assertRaises(loopingcall.LoopingCallTimeOut,
+                          loopingcall.BackOffLoopingCall(false).start()
+                          .wait)
+
+        expected_times = [mock.call(1.6000000000000001),
+                          mock.call(2.5600000000000005),
+                          mock.call(4.096000000000001),
+                          mock.call(6.5536000000000021),
+                          mock.call(10.485760000000004),
+                          mock.call(16.777216000000006),
+                          mock.call(26.843545600000013),
+                          mock.call(42.949672960000022),
+                          mock.call(68.719476736000033),
+                          mock.call(109.95116277760006)]
+        self.assertEqual(expected_times, sleep_mock.call_args_list)
+
+    @mock.patch('random.SystemRandom.gauss')
+    @mock.patch('eventlet.greenthread.sleep')
+    def test_no_backoff(self, sleep_mock, random_mock):
+        random_mock.return_value = 1
+        func = mock.Mock()
+        # func.side_effect
+        func.side_effect = [True, True, True, loopingcall.LoopingCallDone(
+            retvalue='return value')]
+
+        retvalue = loopingcall.BackOffLoopingCall(func).start().wait()
+
+        expected_times = [mock.call(1), mock.call(1), mock.call(1)]
+        self.assertEqual(expected_times, sleep_mock.call_args_list)
+        self.assertTrue(retvalue, 'return value')
+
+    @mock.patch('random.SystemRandom.gauss')
+    @mock.patch('eventlet.greenthread.sleep')
+    def test_no_sleep(self, sleep_mock, random_mock):
+        # Any call that executes properly the first time shouldn't sleep
+        random_mock.return_value = 1
+        func = mock.Mock()
+        # func.side_effect
+        func.side_effect = loopingcall.LoopingCallDone(retvalue='return value')
+
+        retvalue = loopingcall.BackOffLoopingCall(func).start().wait()
+        self.assertFalse(sleep_mock.called)
+        self.assertTrue(retvalue, 'return value')
+
+    @mock.patch('random.SystemRandom.gauss')
+    @mock.patch('eventlet.greenthread.sleep')
+    def test_max_interval(self, sleep_mock, random_mock):
+        def false():
+            return False
+
+        random_mock.return_value = .8
+
+        self.assertRaises(loopingcall.LoopingCallTimeOut,
+                          loopingcall.BackOffLoopingCall(false).start(
+                              max_interval=60)
+                          .wait)
+
+        expected_times = [mock.call(1.6000000000000001),
+                          mock.call(2.5600000000000005),
+                          mock.call(4.096000000000001),
+                          mock.call(6.5536000000000021),
+                          mock.call(10.485760000000004),
+                          mock.call(16.777216000000006),
+                          mock.call(26.843545600000013),
+                          mock.call(42.949672960000022),
+                          mock.call(60),
+                          mock.call(60),
+                          mock.call(60)]
+        self.assertEqual(expected_times, sleep_mock.call_args_list)
+
+
 class AnException(Exception):
     pass
 
