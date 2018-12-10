openstack/oslo.service

commit 2ee3894f49f315e35abff968f54ae72e5480e892

Skips signal handling on Windows
Setting up signal handling fails on Windows due to missing select
module attributes. This patch skips the setup on Windows.

Change-Id: I75db453ec907efb01e1bb411690dadd92a48314d
Closes-Bug: #1788022

oslo_service/service.py
@@ -28,6 +28,7 @@ import io
 import logging
 import os
 import random
+import select
 import signal
 import six
 import sys
@@ -203,7 +204,8 @@ class SignalHandler(object):
         For Python 3.5 and later, deal with the changes in PEP 475 that prevent
         a signal from interrupting eventlet's call to poll() or sleep().
         """
-        self.__force_interrupt_on_signal = sys.version_info >= (3, 5)
+        self.__force_interrupt_on_signal = (sys.version_info >= (3, 5) and
+                                            hasattr(select, 'poll'))
 
         if self.__force_interrupt_on_signal:
             try:

oslo_service/tests/test_service.py
@@ -477,6 +477,18 @@ class ProcessLauncherTest(base.ServiceBaseTestCase):
             m.assert_called_once_with(signal.SIGTERM, 'test')
         signal_handler.clear()
 
+    @mock.patch('sys.version_info', (3, 5))
+    @mock.patch.object(service, 'select', spec=[])
+    def test_setup_signal_interruption_no_select_poll(self, mock_select):
+        # NOTE(claudiub): SignalHandler is a singleton, which means that it
+        # might already be initialized. We need to clear to clear the cache
+        # in order to prevent race conditions between tests.
+        service.SignalHandler.__class__._instances.clear()
+        signal_handler = service.SignalHandler()
+        self.addCleanup(service.SignalHandler.__class__._instances.clear)
+        self.assertFalse(
+            signal_handler._SignalHandler__force_interrupt_on_signal)
+
     @mock.patch('signal.alarm')
     @mock.patch("os.kill")
     @mock.patch("oslo_service.service.ProcessLauncher.stop")

commit 159ef2e1d26f25a5d7a0514d5155f3c74c4a8a86

Restore correct signal handling in Python3
The patch 2ee3894f49f315e35abff968f54ae72e5480e892 broke the original
fix cad75e4e139f734a5138d37ceafa6be169ff4e47 that ensured eventlet could
be interrupted while sleeping after PEP475 was implemented in Python
3.5. Eventlet monkey-patches the signal module with its own version, so
we have to look up the original module to determine whether the
underlying OS actually supports the poll() function.

Change-Id: Ia712c9a83d8081bf0b5e6fe36f169f9028aae3dc
Closes-Bug: #1803731
Related-Bug: #1788022
Related-Bug: #1705047

oslo_service/service.py
@@ -28,7 +28,6 @@ import io
 import logging
 import os
 import random
-import select
 import signal
 import six
 import sys
@@ -204,8 +203,9 @@ class SignalHandler(object):
         For Python 3.5 and later, deal with the changes in PEP 475 that prevent
         a signal from interrupting eventlet's call to poll() or sleep().
         """
+        select_module = eventlet.patcher.original('select')
         self.__force_interrupt_on_signal = (sys.version_info >= (3, 5) and
-                                            hasattr(select, 'poll'))
+                                            hasattr(select_module, 'poll'))
 
         if self.__force_interrupt_on_signal:
             try:

oslo_service/tests/test_service.py
@@ -478,15 +478,28 @@ class ProcessLauncherTest(base.ServiceBaseTestCase):
         signal_handler.clear()
 
     @mock.patch('sys.version_info', (3, 5))
-    @mock.patch.object(service, 'select', spec=[])
-    def test_setup_signal_interruption_no_select_poll(self, mock_select):
+    def test_setup_signal_interruption_no_select_poll(self):
+        # NOTE(claudiub): SignalHandler is a singleton, which means that it
+        # might already be initialized. We need to clear to clear the cache
+        # in order to prevent race conditions between tests.
+        service.SignalHandler.__class__._instances.clear()
+        with mock.patch('eventlet.patcher.original',
+                        return_value=object()) as get_original:
+            signal_handler = service.SignalHandler()
+            get_original.assert_called_with('select')
+        self.addCleanup(service.SignalHandler.__class__._instances.clear)
+        self.assertFalse(
+            signal_handler._SignalHandler__force_interrupt_on_signal)
+
+    @mock.patch('sys.version_info', (3, 5))
+    def test_setup_signal_interruption_select_poll(self):
         # NOTE(claudiub): SignalHandler is a singleton, which means that it
         # might already be initialized. We need to clear to clear the cache
         # in order to prevent race conditions between tests.
         service.SignalHandler.__class__._instances.clear()
         signal_handler = service.SignalHandler()
         self.addCleanup(service.SignalHandler.__class__._instances.clear)
-        self.assertFalse(
+        self.assertTrue(
             signal_handler._SignalHandler__force_interrupt_on_signal)
 
     @mock.patch('signal.alarm')