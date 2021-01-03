e37c5d2aff49677688715f5db6bd2ee4a0ef58b8
xuanyandong <xuanyandong@inspur.com>
2021-01-03 16:21:39 +0800

    remove unicode from code
    Change-Id: I4cf57785a3d2e6121fbb470b57a1ba48a3549ef6

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,8 +45,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'oslo.service'
-copyright = u'2014, OpenStack Foundation'
+project = 'oslo.service'
+copyright = '2014, OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -76,6 +76,6 @@ htmlhelp_basename = '%sdoc' % project
 latex_documents = [
     ('index',
      '%s.tex' % project,
-     u'%s Documentation' % project,
-     u'OpenStack Foundation', 'manual'),
+     '%s Documentation' % project,
+     'OpenStack Foundation', 'manual'),
 ]
